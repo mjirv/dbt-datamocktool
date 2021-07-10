ab5beba91e80720d0c03cd1c75f1485f19654747
Michael Irvine <michael.j.irvine@gmail.com>
2021-07-10 11:10:43 -0400

    fix test name

--- macros/dmt_unit_test.sql
+++ macros/dmt_unit_test.sql
@@ -1,3 +1,3 @@
-{% macro test_dbt_unit_test(model, expected_output) %}
+{% macro test_unit_test(model, expected_output) %}
     {{ dbt_utils.test_equality(model, compare_model=expected_output) }}
 {% endmacro %}
