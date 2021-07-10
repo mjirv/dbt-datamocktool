03858f62e63a1b68d84a3d31960a81595c65c11f
Michael Irvine <michael.j.irvine@gmail.com>
2021-07-10 11:08:07 -0400

    eval isn't it, boss

--- macros/dmt_ref.sql
+++ macros/dmt_ref.sql
@@ -1,8 +1,8 @@
 {% macro ref(model_name) %}
     {% if var('dmt_test_suite', '') != '' %}
-        {% set mapping_dict = "var('dmt_mappings')[var('dmt_test_suite')]['refs']" %}
-        {% if model_name in eval(mapping_dict) %}
-            {% do return(builtins.ref(eval(mapping_dict)[model_name])) %}
+        {% set mapping_dict = var('dmt_mappings')[var('dmt_test_suite')]['refs'] %}
+        {% if model_name in mapping_dict %}
+            {% do return(builtins.ref(mapping_dict[model_name])) %}
         {% endif %}
     {% endif %}
 
--- macros/dmt_source.sql
+++ macros/dmt_source.sql
@@ -1,9 +1,9 @@
 {% macro source(schema_name, source_name) %}
     {% if var('dmt_test_suite', '') != '' %}
-        {% set mapping_dict = "var('dmt_mappings')[var('dmt_test_suite')]['sources']" %}
-        {% if schema_name in eval(mapping_dict) %}
-            {% if model_name in eval(mapping_dict)[schema_name] %}
-                {% do return(builtins.ref(eval(mapping_dict)[schema_name][source_name])) %}
+        {% set mapping_dict = var('dmt_mappings')[var('dmt_test_suite')]['sources'] %}
+        {% if schema_name in mapping_dict %}
+            {% if model_name in mapping_dict[schema_name] %}
+                {% do return(builtins.ref(mapping_dict[schema_name][source_name])) %}
             {% endif %}
         {% endif %}
     {% endif %}
