8b9182a39107eb18ab638c1c26fa591fa93572b0
Michael Irvine <michael.j.irvine@gmail.com>
2021-07-14 11:56:12 -0400

    update READMEs and integration test script

--- integration_tests/run_integration_tests.sh
+++ integration_tests/run_integration_tests.sh
@@ -1,2 +1,1 @@
-dbt deps && dbt seed && dbt run --vars "dmt_test_suite: test_suite_1" && dbt test -m tag:test_suite_1 || exit 1
-dbt seed && dbt run && dbt test --exclude tag:test_suite_1 || exit 1
+dbt deps && dbt seed && dbt test
