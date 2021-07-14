897e2ae0d1de597fb041ef552dd7f162dea46382
Michael Irvine <michael.j.irvine@gmail.com>
2021-07-14 12:49:58 -0400

    use kwarg for compatibility with older versions of dbt_utils

--- macros/dmt_unit_test.sql
+++ macros/dmt_unit_test.sql
@@ -1,5 +1,5 @@
 {% macro test_unit_test(model, input_mapping, expected_output) %}
     {% set test_sql = dbt_datamocktool.get_unit_test_sql(model, input_mapping) %}
 
-    {% do return(dbt_utils.test_equality(expected_output, test_sql)) %}
+    {% do return(dbt_utils.test_equality(expected_output, compare_model=test_sql)) %}
 {% endmacro %}
