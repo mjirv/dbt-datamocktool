2516f5254b6ecfe16d4e124abdeadc3e589b2d93
Petter Söderlund <petter.soderlund@avanza.se>
2022-11-03 12:08:17 +0100

    working code for incremental tests -- old unit test still works

--- macros/dmt_get_test_sql.sql
+++ macros/dmt_get_test_sql.sql
@@ -23,18 +23,50 @@
         {% endif %}
         {% set ns.test_sql = ns.graph_model.raw_code %}
 
+        {# before rendering -- check if test should be incremental #}
+        {# suppose it is incremental if this is in input mapping #}
+        {% if 'this' in input_mapping.keys() %}    
+            {% set ns.test_sql = ns.test_sql|replace('is_incremental()','true') %}     
+        {% endif %}
+
         {% for k,v in input_mapping.items() %}
             {# render the original sql and replacement key before replacing because v is already rendered when it is passed to this test #}
             {% set ns.test_sql = render(ns.test_sql)|replace(ns.rendered_keys[k], v) %}
         {% endfor %}
 
+        {# after rendering .. replace this with project dataset and mocked ""this"" model #}
+        {% if 'this' in input_mapping.keys() %}    
+            {% set ns.test_sql = ns.test_sql|replace(this.dataset, model.dataset) %}     
+            {% set ns.test_sql = ns.test_sql|replace(this.table, input_mapping.this) %}     
+        {% endif %}
+
+        {# mock_model_relation is the mocked model name #}
         {% set mock_model_relation = dbt_datamocktool._get_model_to_mock(
             model, suffix=('_dmt_' ~ modules.datetime.datetime.now().strftime("%S%f"))
         ) %}
 
-        {% do dbt_datamocktool._create_mock_table_or_view(mock_model_relation, ns.test_sql) %}
-    {% endif %}
+        {{ log('var: ' ~ mock_model_relation, info=True) }}
 
+        {# TODO create merge instead of create table or view if its an incremental test  #}
+        {# if incremtental test then #}
+            {# create base model from input  (copy seed into target table?) #}
+
+        {% if 'this' in input_mapping.keys() %}    
+            {# create base model from input  (copy seed into target table?) #}
+            {{ log("copying base table", info=True) }}
+            {% do dbt_datamocktool._create_mock_table_or_view(mock_model_relation, "select * from " ~ input_mapping.this) %}
+            
+            {# create merge statement to merge into base model #}
+            {{ log("creating merge statement", info=True) }}
+            {# need sql to be wrapped in parentheses  - see bq_generate_incremental_build_sql #}
+            {% do dbt_datamocktool._create_mock_merge_table(mock_model_relation, "(" + ns.test_sql + ")", dest_columns=adapter.get_columns_in_relation(mock_model_relation)) %}
+        {% else %}
+            {# normal run #}
+            {{ log("normal run", info=True) }}
+            {% do dbt_datamocktool._create_mock_table_or_view(mock_model_relation, ns.test_sql) %}
+        {% endif %}
+
+    {% endif %}
     {% for k in depends_on %}
         -- depends_on: {{ k }}
     {% endfor %}
@@ -79,4 +111,12 @@
 
 {% macro sqlserver___create_mock_table_or_view(model, test_sql) %}
     {% do run_query(create_view_as(model, test_sql)) %}
+{% endmacro %}
+
+{% macro _create_mock_merge_table(model, test_sql, dest_columns) %}
+    {{ return(adapter.dispatch('_create_mock_merge_table', 'dbt_datamocktool')(model, test_sql, dest_columns)) }}
+{% endmacro %}
+
+{% macro default___create_mock_merge_table(model, test_sql, dest_columns) %}
+    {% do run_query(get_merge_sql(model, test_sql, dest_columns=dest_columns)) %}
 {% endmacro %}
