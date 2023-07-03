8190f7b4c737eca3b3cfe4f1125feb055513cfb2
Michael Irvine <michael.j.irvine@gmail.com>
2023-07-03 17:33:35 -0400

    thought i got rid of that already

--- macros/dmt_unit_test.sql
+++ macros/dmt_unit_test.sql
@@ -4,10 +4,6 @@
     {{ test_report }}
 {%- endtest -%}
 
-{%- test unit_test(model, input_mapping, expected_output, name, description, compare_columns, depends_on) -%}
-    {{ dbt_datamocktool.unit_test(model, input_mapping, expected_output, name, description, compare_columns, depends_on) }}
-{%- endtest -%}
-
 {% test unit_test_incremental(model, input_mapping, expected_output, name, description, compare_columns, exclude_columns, depends_on) %}
     {%- set test_sql = dbt_datamocktool.get_unit_test_incremental_sql(model, input_mapping, depends_on)|trim -%}
     {%- set test_report = dbt_datamocktool.test_equality(expected_output, name, compare_model=test_sql, compare_columns=compare_columns, exclude_columns=exclude_columns) -%}
