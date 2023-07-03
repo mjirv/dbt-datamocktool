6384b3b44ef255badfc2e364b17fb12c5350bab4
Michael Irvine <michael.j.irvine@gmail.com>
2023-07-03 17:38:15 -0400

    o i see

--- macros/dmt_unit_test.sql
+++ macros/dmt_unit_test.sql
@@ -1,14 +1,22 @@
-{%- test unit_test(model, input_mapping, expected_output, name, description, compare_columns, exclude_columns, depends_on) -%}
+{%- macro unit_test(model, input_mapping, expected_output, name, description, compare_columns, exclude_columns, depends_on) -%}
     {%- set test_sql = dbt_datamocktool.get_unit_test_sql(model, input_mapping, depends_on)|trim -%}
     {%- set test_report = dbt_datamocktool.test_equality(expected_output, name, compare_model=test_sql, compare_columns=compare_columns, exclude_columns=exclude_columns) -%}
     {{ test_report }}
-{%- endtest -%}
+{%- endmacro -%}
 
-{% test unit_test_incremental(model, input_mapping, expected_output, name, description, compare_columns, exclude_columns, depends_on) %}
+{%- test unit_test(model, input_mapping, expected_output, name, description, compare_columns, depends_on) -%}
+     {{ dbt_datamocktool.unit_test(model, input_mapping, expected_output, name, description, compare_columns, depends_on) }}
+ {%- endtest -%}
+
+{% macro unit_test_incremental(model, input_mapping, expected_output, name, description, compare_columns, exclude_columns, depends_on) %}
     {%- set test_sql = dbt_datamocktool.get_unit_test_incremental_sql(model, input_mapping, depends_on)|trim -%}
     {%- set test_report = dbt_datamocktool.test_equality(expected_output, name, compare_model=test_sql, compare_columns=compare_columns, exclude_columns=exclude_columns) -%}
     {{ test_report }}
-{% endtest %}
+{% endmacro %}
+
+{% test unit_test_incremental(model, input_mapping, expected_output, name, description, compare_columns, depends_on) %}
+     {{ dbt_datamocktool.unit_test_incremental(model, input_mapping, expected_output, name, description, compare_columns, depends_on) }}
+ {% endtest %}
 
 {%- macro test_equality(model, name, compare_model, compare_columns=[], exclude_columns=[]) -%}
 
