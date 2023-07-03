be9f2596bc29a1db2b4ca0557ef0edc556175d3e
Michael Irvine <michael.j.irvine@gmail.com>
2023-07-03 17:30:17 -0400

    oops

--- macros/dmt_unit_test.sql
+++ macros/dmt_unit_test.sql
@@ -2,7 +2,7 @@
     {%- set test_sql = dbt_datamocktool.get_unit_test_sql(model, input_mapping, depends_on)|trim -%}
     {%- set test_report = dbt_datamocktool.test_equality(expected_output, name, compare_model=test_sql, compare_columns=compare_columns, exclude_columns=exclude_columns) -%}
     {{ test_report }}
-{%- endmacro -%}
+{%- endtest -%}
 
 {%- test unit_test(model, input_mapping, expected_output, name, description, compare_columns, depends_on) -%}
     {{ dbt_datamocktool.unit_test(model, input_mapping, expected_output, name, description, compare_columns, depends_on) }}
